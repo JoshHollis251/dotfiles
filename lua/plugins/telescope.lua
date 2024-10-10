c4dd5972ad521dd9242b6745b64989a067d51384
Josh Hollis <josh.hollis@garmin.com>
2024-10-10 17:57:44 -0400

    add new telescope function to search directory
    Change-Id: Ib097b9a5e74a43d444bc43e1dcc6751fef961b29

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,12 +5,24 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
+      --Custom commands
+      --telescope a specific directory
+      vim.api.nvim_create_user_command('TelescopeDir', function()
+        local input_dir = vim.fn.input('Enter directory: ')
+        builtin.find_files({
+          search_dirs = { input_dir },
+          hidden = true,  -- Include hidden files
+        })
+      end, {})
+      --End custom commands
+
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "find files" })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "live grep" })
       vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = "prev grep session" })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "find buffers" })
       vim.keymap.set('n', '<leader>fm', builtin.man_pages, { desc = "man pages" })
       vim.keymap.set('n', '<leader>fs', builtin.search_history, { desc = "search history" })
+      vim.keymap.set('n', '<leader>fd', ':TelescopeDir<CR>', { noremap = true, silent = true, desc = "Find Files in Directory" })
     end
   },
   {
